af6c56520d6b4e1ebc3c80f40e1fd18b19f8e270
Craig Miller <craigmiller160@gmail.com>
2022-11-04 17:31:21 -0400

    Implemented the rollback stuff

--- src/main/kotlin/io/craigmiller160/flywayrollback/domain/repository/FlywaySchemaHistoryRepository.kt
+++ src/main/kotlin/io/craigmiller160/flywayrollback/domain/repository/FlywaySchemaHistoryRepository.kt
@@ -2,7 +2,14 @@ package io.craigmiller160.flywayrollback.domain.repository
 
 import io.craigmiller160.flywayrollback.domain.entity.FlywaySchemaHistory
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.stereotype.Repository
+import javax.transaction.Transactional
 
 @Repository
-interface FlywaySchemaHistoryRepository : JpaRepository<FlywaySchemaHistory, Int>
+interface FlywaySchemaHistoryRepository : JpaRepository<FlywaySchemaHistory, Int> {
+    fun findByScript(script: String): FlywaySchemaHistory?
+    @Transactional
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    fun deleteByScript(script: String)
+}
--- src/main/kotlin/io/craigmiller160/flywayrollback/web/controller/FlywayController.kt
+++ src/main/kotlin/io/craigmiller160/flywayrollback/web/controller/FlywayController.kt
@@ -1,29 +1,50 @@
 package io.craigmiller160.flywayrollback.web.controller
 
+import io.craigmiller160.flywayrollback.domain.repository.FlywaySchemaHistoryRepository
 import io.craigmiller160.flywayrollback.web.types.UndoRequest
-import io.craigmiller160.flywayrollback.web.types.UndoResponse
-import io.craigmiller160.flywayrollback.web.types.UndoScriptResponse
-import io.craigmiller160.flywayrollback.web.types.UndoStatus
 import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpStatus
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import java.lang.RuntimeException
+import java.nio.charset.StandardCharsets
+import java.sql.Connection
 import javax.sql.DataSource
+import javax.transaction.Transactional
 
 @RestController
 @RequestMapping("/flyway")
 class FlywayController(
-    private val dataSource: DataSource
+    private val dataSource: DataSource,
+    private val flywaySchemaHistoryRepository: FlywaySchemaHistoryRepository
 ) {
     @PostMapping("/undo")
-    fun undo(@RequestBody request: UndoRequest): UndoResponse {
-        val (validScripts, invalidScripts) = request.scripts
-            .map { it to ClassPathResource("db/migration/$it") }
-            .partition { it.second.exists() }
-        return UndoResponse(
-            scripts = validScripts.associate { it.first to UndoScriptResponse(UndoStatus.SUCCESS) } +
-                invalidScripts.associate { it.first to UndoScriptResponse(UndoStatus.FAILURE, "Invalid script path") }
-        )
+    @Transactional
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun undo(@RequestBody request: UndoRequest) {
+        dataSource.connection.use { conn ->
+            conn.autoCommit = false
+            request.scripts.forEach { findAndExecuteRollbackScript(conn, it) }
+            conn.commit()
+        }
+    }
+
+    // TODO validate transactional nature of all this
+    private fun findAndExecuteRollbackScript(connection: Connection, script: String) {
+        val record = flywaySchemaHistoryRepository.findByScript(script) ?: throw RuntimeException("Script was never executed")
+
+        val undoScript = script.replace(Regex("^V"), "U")
+        val resource = ClassPathResource("db/migration/$undoScript")
+        if (!resource.exists()) {
+            throw RuntimeException("Undo script does not exist on classpath: $undoScript")
+        }
+
+        ResourceDatabasePopulator(false, false, StandardCharsets.UTF_8.name(), resource)
+            .populate(connection)
+        flywaySchemaHistoryRepository.deleteByScript(script)
     }
 }
