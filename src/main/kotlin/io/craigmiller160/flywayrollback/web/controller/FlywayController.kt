8fd317b15ee3c5e0f135552675241bcaabed7b6b
Craig Miller <craigmiller160@gmail.com>
2022-11-04 17:20:12 -0400

    Fixed bug

--- src/main/kotlin/io/craigmiller160/flywayrollback/web/controller/FlywayController.kt
+++ src/main/kotlin/io/craigmiller160/flywayrollback/web/controller/FlywayController.kt
@@ -6,6 +6,7 @@ import io.craigmiller160.flywayrollback.web.types.UndoScriptResponse
 import io.craigmiller160.flywayrollback.web.types.UndoStatus
 import org.springframework.core.io.ClassPathResource
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import javax.sql.DataSource
@@ -16,7 +17,7 @@ class FlywayController(
     private val dataSource: DataSource
 ) {
     @PostMapping("/undo")
-    fun undo(request: UndoRequest): UndoResponse {
+    fun undo(@RequestBody request: UndoRequest): UndoResponse {
         val (validScripts, invalidScripts) = request.scripts
             .map { it to ClassPathResource("db/migration/$it") }
             .partition { it.second.exists() }
